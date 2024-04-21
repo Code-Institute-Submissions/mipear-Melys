3c9180675191bfe6ac1006aa9f13b6b37bafc602
Emilia Pearse <mili.pearse@gmail.com>
2024-04-21 16:13:05 +0000

    Added stripe public key and sk

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -10,11 +14,20 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
+        'stripe_public_key': 'pk_test_51P7wJmHi5rVyeR2vzjg7JTD4cvvUcRYiwEH1ayT39gy4wDjsZ9bcFjGVDrAptHBBdPZS4GbzOKPvaGvP3kpjduZd00P05Qsr5w',
         'client_secret': 'test client secret',
     }
 
