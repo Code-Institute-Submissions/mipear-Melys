8bb8652beca3d13cc16d67106cb966d7492b3145
Emilia Pearse <mili.pearse@gmail.com>
2024-04-27 14:00:50 +0000

    Customised workshop admin

--- workshops/admin.py
+++ workshops/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import Workshops
 
 # Register your models here.
-admin.site.register(Workshops)
+
+class WorkshopsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'duration',
+        'date',
+        'time',
+        'location',
+        'image',
+    )
+
+admin.site.register(Workshops, WorkshopsAdmin)
