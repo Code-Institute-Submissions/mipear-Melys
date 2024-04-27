a1c29167d07730e6019305abba241b87fefd25f5
Emilia Pearse <mili.pearse@gmail.com>
2024-04-27 14:22:03 +0000

    Added workshops views

--- workshops/views.py
+++ workshops/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Workshops
 
 # Create your views here.
 
 def view_workshops(request):
     """ A view that renders the workshops page """
 
-    return render(request, 'workshops/workshops.html')
+    workshops = Workshops.objects.all()
+
+    context = {
+        'workshops': workshops,
+    }
+
+    return render(request, 'workshops/workshops.html', context)
