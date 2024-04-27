620ec2e645f49b81d0c34d0d708d150348bb3a5f
Emilia Pearse <mili.pearse@gmail.com>
2024-04-27 13:24:57 +0000

    Added Workshops model

--- workshops/models.py
+++ workshops/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Workshops(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    duration = models.CharField(max_length=50)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+      return self.name
