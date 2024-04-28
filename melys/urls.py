abccaee16dbc74d7bcd01bd282f79eaee1fc6f70
Emilia Pearse <mili.pearse@gmail.com>
2024-04-28 15:02:30 +0000

    Changed url path for workshopapply

--- melys/urls.py
+++ melys/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('workshops/', include('workshops.urls')),
+    path('workshopapply/', include('workshops.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- workshops/urls.py
+++ workshops/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_workshops, name='view_workshops'),
-    path('', views.workshopapply, name='workshopapply')
+    path('workshopapply', views.workshopapply, name='workshopapply'),
 ]
