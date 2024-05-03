b0cfb6221a044be975c37cd890543f0aef539176
Emilia Pearse <mili.pearse@gmail.com>
2024-05-03 15:49:06 +0000

    Deploying to Heroku

--- melys/settings.py
+++ melys/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-hj7%+_6jmjdhw7*6312k94+y-2i#7r@tp2er62s1sc_tzfl9t_
 DEBUG = True
 
 # ALLOWED_HOSTS = ['8000-mipear-melys-yhp8si6ogfj.ws-eu110.gitpod.io']
-ALLOWED_HOSTS = ['8000-mipear-melys-dfbitboct9b.ws-eu112.gitpod.io']
+ALLOWED_HOSTS = ['melys.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -110,7 +111,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none' #'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -122,12 +123,17 @@ WSGI_APPLICATION = 'melys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
